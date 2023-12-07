7310bb6e0933295101c14ad579a65fe847536fe0
AODHOffice <140739508+AODHOffice@users.noreply.github.com>
2023-12-07 17:36:16 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,159 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6632090998939226,
+          "pitch": 0.1677108137031933,
+          "rotation": 0,
+          "target": "3-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dining-room",
+      "name": "Dining Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5046930445016837,
+          "pitch": 0.27330127869637444,
+          "rotation": 0,
+          "target": "0-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dressing-room",
+      "name": "Dressing Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04121381165192162,
+          "pitch": 0.30514495040318934,
+          "rotation": 0,
+          "target": "1-dining-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3742750010968052,
+          "pitch": 0.18165702764052583,
+          "rotation": 0,
+          "target": "2-dressing-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
